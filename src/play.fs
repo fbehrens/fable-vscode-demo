a0ed9cd3937f8835a18001ddffd624d9f9467d1e
Frank Behrens <fbehrens@gmail.com>
2018-08-25 12:02:46 +0200

    include another file

---
+++ src/play.fs
@@ -0,0 +1,23 @@
+namespace Polyglott
+module Play  =
+  open System
+  open Fable.Core
+  open Fable.Import.vscode
+  open Ionide.VSCode.Helpers
+  
+  let hello () =
+    promise {
+      let! a= window.showInformationMessage("Hello4", "a" ,"b")
+      printfn "chosen3 %A" a
+    } |> ignore
+
+  // let showQuickPick () = 
+  //   promise {
+  //     let ra = ["essen";"trinken"] |>ResizeArray 
+  //     let! r = window.showQuickPick( ra |> Case1)
+  //     printfn "selected %s" r } 
+
+  let activate (context : ExtensionContext) = 
+    commands.registerCommand("polyglott.hello", hello |> unbox<Func<obj,obj>> )
+    |> context.subscriptions.Add 
+
