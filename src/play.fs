7a70d108ab12a199cc6e2149a0f625476a7af548
Frank Behrens <fbehrens@gmail.com>
2018-08-25 13:19:24 +0200

    Quickpick

--- src/play.fs
+++ src/play.fs
@@ -2,6 +2,7 @@ namespace Polyglott
 module Play  =
   open System
   open Fable.Core
+  open Fable.Core.JsInterop
   open Fable.Import
   open Ionide.VSCode.Helpers
   
@@ -14,18 +15,22 @@ module Play  =
   let hello () =
     vscode.window.showInformationMessage "Hello" |> ignore
 
-  // let quickPick () = 
-  //   promise {
-  //     let ra = ["essen";"trinken"] |>ResizeArray 
-  //     let! r = window.showQuickPick( ra |> Case1)
-  //     printfn "selected %s" r } 
+  let quickPick () = 
+    promise {
+      let ra = ["essen";"trinken"] |>ResizeArray 
+      let opts = createEmpty<vscode.QuickPickOptions>
+      let! r = vscode.window.showQuickPick( ra |> U2.Case1, opts)
+      printfn "selected  %s" r 
+    } |> ignore
 
   let activate (context : vscode.ExtensionContext) = 
     [ 
       "polyglott.choose", choose
       "polyglott.hello", hello
+      "polyglott.quickPick", quickPick
     ]
     |> List.iter (fun (name, f) -> 
+          printfn "regsterCommand %s" name
           vscode.commands.registerCommand(name, f |> unbox<Func<obj,obj>> )
           |> context.subscriptions.Add
        )
