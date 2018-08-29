2f0fcdd2b45880301d46b347ed28a174546c7ea0
Frank Behrens <fbehrens@gmail.com>
2018-08-29 13:40:07 +0200

    build5 finished

--- build5.fsx
+++ build5.fsx
@@ -1,5 +1,4 @@
 #r "paket: groupref build5 //"
-open Fake.DotNet
 #load "./.fake/build5.fsx/intellisense.fsx"
 
 open Fake.Core
@@ -7,9 +6,10 @@ open Fake.IO
 open Fake.JavaScript
 open Fake.DotNet
 
-Target.create "Clean" (fun _ ->
+Target.create "Clean" <| fun _ ->
   Shell.cleanDir "./temp"
-)
+  Shell.copy "release" ["README.md"; "LICENSE.md"]
+  Shell.copyFile "release/CHANGELOG.md" "RELEASE_NOTES.md"
 
 Target.description "Install javaScript dependencies"
 Target.create "YarnInstall" <| fun _ ->
@@ -18,11 +18,26 @@ Target.create "YarnInstall" <| fun _ ->
 Target.create "DotnetRestore" <| fun _ ->
     DotNet.restore (DotNet.Options.withWorkingDirectory("src")) ""
 
-// let runFable additionalArgs =
-//     let cmd = "fable webpack -- --config ./webpack.config.js " + additionalArgs
-//     DotNetCli.RunCommand (fun p -> { p with WorkingDir = "src" } ) cmd
+let runFable additionalArgs =
+    let cmd = "fable webpack -- --config ./webpack.config.js " 
+    let r = DotNet.exec (DotNet.Options.withWorkingDirectory("src")) cmd additionalArgs
+    printfn "result=%A" r
 
+Target.create "RunScript" <| fun _ ->
+    // Ideally we would want a production (minized) build but UglifyJS fail on PerMessageDeflate.js as it contains non-ES6 javascript.
+    runFable ""
+
+Target.create "Watch" <| fun _ ->
+    runFable "--watch"
 
 Target.create "Default" ignore
+
+open Fake.Core.TargetOperators
+"YarnInstall" ?=> "RunScript"
+"DotNetRestore" ?=> "RunScript"
+
+"Clean"
+==> "RunScript"
+==> "Default"
 
 Target.runOrDefault "Default"
