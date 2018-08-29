43957523b3ad868d8c8c4f10430c925e2d190ff8
Frank Behrens <fbehrens@gmail.com>
2018-08-29 13:10:46 +0200

    Dotnet Restore with fake5

---
+++ build5.fsx
@@ -0,0 +1,31 @@
+#r "paket: groupref build5 //"
+open Fake.DotNet
+#load "./.fake/build5.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.IO
+open Fake.JavaScript
+open Fake.DotNet
+
+Target.create "Clean" (fun _ ->
+  Shell.cleanDir "./temp"
+)
+
+Target.description "Install javaScript dependencies"
+Target.create "YarnInstall" <| fun _ ->
+    Yarn.install id
+
+Target.create "DotnetRestore" <| fun _ ->
+    DotNet.restore (fun o -> {o with 
+                                Common = {DotNet.Options.Create() with 
+                                            WorkingDirectory = "src"}} ) ""
+
+
+// let runFable additionalArgs =
+//     let cmd = "fable webpack -- --config ./webpack.config.js " + additionalArgs
+//     DotNetCli.RunCommand (fun p -> { p with WorkingDir = "src" } ) cmd
+
+
+Target.create "Default" ignore
+
+Target.runOrDefault "Default"
